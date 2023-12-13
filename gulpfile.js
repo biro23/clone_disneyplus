1bbcef0367cdd1f174ba2f07eb3ff5f8464b603e
biro23 <121694408+biro23@users.noreply.github.com>
2023-12-13 12:29:47 -0300

    adicionando fotos na section

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // importando o gulp, importar tambem o SASS
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require('gulp-imagemin');  
 
 // funcao abaixo recupera os arquivos, .pipe(sass) - chamando o sass
 function styles() {
@@ -10,8 +11,18 @@ function styles() {
     .pipe(gulp.dest("./dist/css"));
 }
 
+// *** para pegar todos os arquivos dentro da pasta images 
+function images() {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));  
+}
+
 // dentro do gulp.watch = passa os arquivos que serao observados
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
+
 exports.watch = function(){
   gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
 }
